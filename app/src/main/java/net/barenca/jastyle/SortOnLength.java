021627357e55a442dc7406d16415c82624d948fb
Trần Lê Duy <tranleduy1233@gmail.com>
2017-07-16 22:37:29 +0700

    add formatter

--- app/src/main/java/net/barenca/jastyle/SortOnLength.java
+++ app/src/main/java/net/barenca/jastyle/SortOnLength.java
@@ -42,6 +42,6 @@ class SortOnLength implements Serializable,Comparator<String>
 
 	public int compare(String a, String b)
 	{
-		return a.length() - b.length();
+		return -(a.length() - b.length());
 	}
 }
--- app/src/test/java/com/duy/editor/editor/TaskFormatCodeTest.java
+++ app/src/test/java/com/duy/editor/editor/TaskFormatCodeTest.java
@@ -17,19 +17,15 @@ public class TaskFormatCodeTest extends TestCase {
 
 
     public void test3() throws IOException {
-        String source = "package com.example;\n" +
-                "\n" +
-                "import javList;\n" +
-                "\n" +
-                "public class Main {public static void main(String[] args) {NumberOne numberOne = new NumberOne();numberOne.print();NumberTwo numberTwo = new NumberTwo();ArrayList<String> arrayList = new ArrayList<String>();arrayList.add(numberOne.toString());arrayList.add(numberTwo.toString());System.out.println(arrayList); for (int i = 0; i < 10; i++){}}\n" +
-                "}\n";
+        String source = "{\n{\n{\n}\n}\n}\n";
         ASFormatter formatter = new ASFormatter();
-
-        // bug on lib's implementation. reported here: http://barenka.blogspot.com/2009/10/source-code-formatter-library-for-java.html
-        source = source.replace("{", "{\n");
+        formatter.setBreakOneLineBlocksMode(true);
+        formatter.setBreakBlocksMode(true);
+        formatter.setBreakClosingHeaderBlocksMode(true);
+        formatter.setJavaStyle();
+        source = source + "\n";
 
         Reader in = new BufferedReader(new StringReader(source));
-        formatter.setJavaStyle();
 
         String format = FormatterHelper.format(in, formatter);
         System.out.println(format);

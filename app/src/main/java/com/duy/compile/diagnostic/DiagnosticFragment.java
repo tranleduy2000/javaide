81722e336a9cec196e695aa3da854237d754d4fb
duy <tranleduy1233>
2017-07-19 23:36:26 +0700

    add scrollbar for recycle view

--- app/src/main/java/com/duy/compile/diagnostic/DiagnosticFragment.java
+++ app/src/main/java/com/duy/compile/diagnostic/DiagnosticFragment.java
@@ -3,6 +3,7 @@ package com.duy.compile.diagnostic;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -49,7 +50,8 @@ public class DiagnosticFragment extends Fragment implements DiagnosticContract.V
         mAdapter = new DiagnosticAdapter(getContext(), new ArrayList<Diagnostic>());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mRecyclerView.setAdapter(mAdapter);
-
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
         mAdapter.setListener(new DiagnosticAdapter.OnItemClickListener() {
             @Override
             public void onClick(Diagnostic diagnostic) {

9185ba76587dbeeb6437834fadda2eddc002e098
Trần Lê Duy <tranleduy1233@gmail.com>
2017-08-05 15:44:58 +0700

    add library

---
+++ app/src/main/java/com/spartacusrex/spartacuside/external/apkbuilder.java
@@ -0,0 +1,13 @@
+package com.spartacusrex.spartacuside.external;
+
+import com.android.sdklib.build.ApkBuilderMain;
+
+/**
+ * Created by Duy on 05-Aug-17.
+ */
+
+public class apkbuilder {
+    public static void main(String[] ars) {
+        ApkBuilderMain.main(ars);
+    }
+}
--- app/src/main/java/com/spartacusrex/spartacuside/external/jarsigner.java
+++ app/src/main/java/com/spartacusrex/spartacuside/external/jarsigner.java
@@ -5,13 +5,6 @@
 
 package com.spartacusrex.spartacuside.external;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import kellinwood.security.zipsigner.ZipSigner;
-
 /**
  * @author Spartacus Rex
  */
@@ -24,65 +17,65 @@ public class jarsigner {
     }
 
     public static void main(String[] zArgs) {
-        try {
-            //Check args
-            if (zArgs.length != 2) {
-                usage();
-                System.exit(1);
-            }
-
-            ZipSigner zipsigner = new ZipSigner();
-
-            zipsigner.setKeymode(ZipSigner.KEY_TESTKEY);
-
-            //Create URL
-            //URL keystoreUrl=new URL("");
-
-            //Get the default asset
-            //String keystore="/assets/keystore.ks";
-            //keystoreUrl = keystoreUrl.getClass().getResource( keystore);
-
-            zipsigner.signZip(zArgs[0], zArgs[1]);
-
+//        try {
+//            //Check args
+//            if (zArgs.length != 2) {
+//                usage();
+//                System.exit(1);
+//            }
 //
-//              if (keystore != null) keystoreUrl = new URL( keystore);
-//              else {
-//                  keystore = "/assets/keystore.ks";
-//                  keystoreUrl = getClass().getResource( keystore);
-//                  if (keystoreUrl == null) keystore = "classpath:"+keystore;
-//              }
+//            ZipSigner zipsigner = new ZipSigner();
 //
-//              if (keystoreUrl == null) throw new IllegalArgumentException("Unable to locate keystore " + keystore);
-//              zipSigner.signZip(keystoreUrl, keystoreType, keystorePass, keyAlias, keyPass, inputFile, outputFile);
-
-
-
-            /*if(zArgs.length>2){
-                zipsigner.setKeymode(zArgs[2]);
-            }else{
-                System.out.println("No key set. testkey used.");
-                zipsigner.setKeymode("testkey");
-            }*/
-
-            /*zipsigner.addProgressListener(new ProgressListener() {
-
-                public void onProgress(ProgressEvent event) {
-                    String message = event.getMessage();
-                    int percentDone = event.getPercentDone();
-                    // log output or update the display here
-                    System.out.println("Message : " + message + " @ " + percentDone + "%");
-                }
-            });
-            zipsigner.signZip(zArgs[0], zArgs[1]);
-            */
-
-            System.out.println("Sign finished..!");
-
-        } catch (IOException ex) {
-            Logger.getLogger(jarsigner.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (GeneralSecurityException ex) {
-            Logger.getLogger(jarsigner.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//            zipsigner.setKeymode(ZipSigner.KEY_TESTKEY);
+//
+//            //Create URL
+//            //URL keystoreUrl=new URL("");
+//
+//            //Get the default asset
+//            //String keystore="/assets/keystore.ks";
+//            //keystoreUrl = keystoreUrl.getClass().getResource( keystore);
+//
+//            zipsigner.signZip(zArgs[0], zArgs[1]);
+//
+////
+////              if (keystore != null) keystoreUrl = new URL( keystore);
+////              else {
+////                  keystore = "/assets/keystore.ks";
+////                  keystoreUrl = getClass().getResource( keystore);
+////                  if (keystoreUrl == null) keystore = "classpath:"+keystore;
+////              }
+////
+////              if (keystoreUrl == null) throw new IllegalArgumentException("Unable to locate keystore " + keystore);
+////              zipSigner.signZip(keystoreUrl, keystoreType, keystorePass, keyAlias, keyPass, inputFile, outputFile);
+//
+//
+//
+//            /*if(zArgs.length>2){
+//                zipsigner.setKeymode(zArgs[2]);
+//            }else{
+//                System.out.println("No key set. testkey used.");
+//                zipsigner.setKeymode("testkey");
+//            }*/
+//
+//            /*zipsigner.addProgressListener(new ProgressListener() {
+//
+//                public void onProgress(ProgressEvent event) {
+//                    String message = event.getMessage();
+//                    int percentDone = event.getPercentDone();
+//                    // log output or update the display here
+//                    System.out.println("Message : " + message + " @ " + percentDone + "%");
+//                }
+//            });
+//            zipsigner.signZip(zArgs[0], zArgs[1]);
+//            */
+//
+//            System.out.println("Sign finished..!");
+//
+//        } catch (IOException ex) {
+//            Logger.getLogger(jarsigner.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (GeneralSecurityException ex) {
+//            Logger.getLogger(jarsigner.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 
 }

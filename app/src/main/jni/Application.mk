f32348a0c8c1413b8f6b1a72bfd9bad2852fd524
Trần Lê Duy <tranleduy1233@gmail.com>
2017-08-05 15:18:03 +0700

    add aapt source

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,3 +1,3 @@
-# ONLY ARMv5TE as x86 machine code not supported by native binaries..
-# APP_ABI := armeabi x86
+APP_STL := gnustl_static
+APP_PLATFORM := android-9
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
---
+++ app/src/main/jni/JNImain.c
@@ -0,0 +1,57 @@
+#include <jni.h>
+#include <string.h>
+#include <stdio.h>
+#include <android/log.h>
+
+#define DEBUG_TAG "JavaIDEdroid"
+
+// any underscore you use in a package/method name must
+// be translated into _1 on the native side.
+// So, com.t_arn.JavaIDEdroid becomes com_t_1arn_JavaIDEdroid
+
+//===================================================================
+jint Java_com_t_1arn_JavaIDEdroid_Aapt_JNImain(JNIEnv * env, jobject this, jstring args)
+//===================================================================
+{
+	jboolean isCopy;
+	const char * szArgs = (*env)->GetStringUTFChars(env, args, &isCopy);
+  char *ptr1, *ptr2;
+	int i, idx, argc=1, len;
+	jint rc = 99;
+	
+	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, "Native method call: JNImain (%s)", szArgs);
+	len=strlen(szArgs);
+	for (i=0; i<len; i++) if (szArgs[i]=='\t') argc++;
+	char * argv[argc];
+	ptr1 = ptr2 = (char*) szArgs;
+	idx = 0;
+	for (i=0; i<len; i++)
+	{
+	  if (*ptr2=='\t') 
+	  {
+	    *ptr2=0;
+	    argv[idx]=ptr1;
+	    idx++;
+	    ptr1=ptr2+1;
+	  }
+	  ptr2++;
+	} // for
+  argv[idx]=ptr1;
+	
+	// redirect stderr and stdout
+  freopen ("/sdcard/.JavaIDEdroid/native_stderr.txt", "w", stderr);
+  freopen ("/sdcard/.JavaIDEdroid/native_stdout.txt", "w", stdout);
+  
+  fprintf (stdout, "Aapt arguments:\n");
+  for (i=1; i<argc; i++) fprintf (stdout, "%s\n",argv[i]);
+  
+  // call aapt
+	rc = main (argc, argv);
+
+  // stopping the redirection
+  fclose (stderr);
+  fclose (stdout);
+	
+	return rc;
+} // JNImain
+//===================================================================

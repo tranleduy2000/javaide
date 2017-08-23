961ab6948dad5acc97e7c2936c5454df179936fd
Trần Lê Duy <tranleduy1233@gmail.com>
2017-08-23 15:07:21 +0700

    Clean project

--- testapplication/src/main/java/com/duy/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.duy;
-
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("This is java file");
-        System.out.println(new NewClass().toString());
-    }
-}

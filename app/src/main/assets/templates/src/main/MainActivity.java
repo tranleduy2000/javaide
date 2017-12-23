021a9d56c7ccd56889651fc8fe70b3712cfe5b86
Duy <tranleduy1233@gmail.com>
2017-12-23 18:57:02 +0700

    update to android 23 platform

--- app/src/main/assets/templates/src/main/MainActivity.java
+++ app/src/main/assets/templates/src/main/MainActivity.java
@@ -3,16 +3,16 @@ package {PACKAGE};
 import android.app.Activity;
 import android.widget.TextView;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import {PACKAGE}.R;
 
-public class {ACTIVITY_NAME} extends Activity {
+public class {ACTIVITY_NAME} extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-        
+
   }
 
 }

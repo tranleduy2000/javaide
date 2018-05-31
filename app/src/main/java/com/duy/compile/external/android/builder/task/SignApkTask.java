c2ea7ad1dfa046723db079c9964078151580bc53
Duy <tranleduy1233@gmail.com>
2018-05-31 18:35:04 +0700

    Sign debug apk

---
+++ app/src/main/java/com/duy/compile/external/android/builder/task/SignApkTask.java
@@ -0,0 +1,53 @@
+package com.duy.compile.external.android.builder.task;
+
+import com.duy.compile.external.android.builder.AndroidBuilder2;
+import com.duy.compile.external.android.builder.BuildType;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import kellinwood.security.zipsigner.ZipSigner;
+
+public class SignApkTask extends BuildTask {
+    private final BuildType type;
+
+    public SignApkTask(AndroidBuilder2 builder, BuildType type) {
+        super(builder);
+        this.type = type;
+    }
+
+    @Override
+    public String getTaskName() {
+        return "Sign " + type + "  apk";
+    }
+
+    @Override
+    public boolean run() throws Exception {
+        if (type == BuildType.DEBUG) {
+            signDebug();
+        } else {
+            signRelease();
+        }
+        return true;
+    }
+
+    private void signRelease() {
+        // TODO: 31-May-18  signRelease
+    }
+
+    private void signDebug() throws IOException, GeneralSecurityException, IllegalAccessException,
+            InstantiationException, ClassNotFoundException {
+        builder.stdout("Sign debug apk");
+
+        String mode = "testkey";
+
+        ZipSigner signer = new ZipSigner();
+        signer.setKeymode(mode);
+
+        String in = project.getApkUnsigned().getAbsolutePath();
+        String out = project.getApkSigned().getAbsolutePath();
+        signer.signZip(in, out);
+
+        builder.stdout("Signed debug apk " + project.getApkUnsigned().getName() + " => " + project.getApkSigned().getName());
+    }
+}

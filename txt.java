5e8814c6391e249a9db178262445d71d979d9834
Duy <tranleduy1233@gmail.com>
2018-06-06 00:38:32 +0700

    update readme

--- txt.java
+++ txt.java
@@ -3,3 +3,4 @@
 ![](https://github.com/tranleduy2000/javaide/blob/master/wiki/images/open_java_example/Screenshot_20180606-003421.png)
 ![](https://github.com/tranleduy2000/javaide/blob/master/wiki/images/open_java_example/Screenshot_20180606-003425.png)
 ![](https://github.com/tranleduy2000/javaide/blob/master/wiki/images/open_java_example/Screenshot_20180606-003432.png)
+![](https://github.com/tranleduy2000/javaide/blob/master/wiki/images/open_java_example/Screenshot_20180606-003439.png)
